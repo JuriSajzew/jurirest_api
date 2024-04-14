e8c488ad2697c83f766969cde4de77870fb67ddd
Juri Sajzew <j.sajzew@outlook.de>
2024-04-14 11:57:54 +0200

    first working version

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
+import datetime
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    created_at = models.DateField(default=datetime.date.today)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ up.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -m "%*"
+git push
+chrome "https://www.pythonanywhere.com/user/jurirestapi/consoles/33336464/"

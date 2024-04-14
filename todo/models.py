c7bef1499302dbe0ca57331395912dcc703cb3f0
Juri Sajzew <j.sajzew@outlook.de>
2024-04-14 12:37:50 +0200

    added user data

--- todo/models.py
+++ todo/models.py
@@ -7,4 +7,4 @@ class Todo(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=300)
     created_at = models.DateField(default=datetime.date.today)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
--- todo/serializers.py
+++ todo/serializers.py
@@ -3,7 +3,12 @@ from rest_framework import serializers
 from .models import Todo
 
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+     class Meta:
+        model = User
+        fields = ('id', 'last_name', 'first_name', 'username', 'email',)
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()    
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at'] #, 'user'
+        fields = ['id', 'title', 'description', 'created_at', 'user',] #, 'user'
